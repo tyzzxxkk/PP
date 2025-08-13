2081b1b02c914230ea9e5ad377eb1e2e46ffccd0
태연 <s2401@e-mirim.hs.kr>
2025-08-13 21:08:03 +0900

    내용 추가

--- w2d3_1.py
+++ w2d3_1.py
@@ -2,3 +2,8 @@ a = "abcdefghijk"
 result = a.upper()
 
 print(result)
+
+# text = "apple"
+# print(text.upper())      # APPLE
+# print(text.lower())      # apple
+# print(text.replace("a", "A"))  # Apple
