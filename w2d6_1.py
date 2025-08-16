12bd6ca64c8973db1a2d3e4cfe2d8d8c2901b31e
태연 <s2401@e-mirim.hs.kr>
2025-08-16 23:57:58 +0900

    주석 추가

--- w2d6_1.py
+++ w2d6_1.py
@@ -1,5 +1,7 @@
+# 학생 한 명을 딕셔너리로 표현
+# key는 'name', value는 학생 이름
 students = [{"name": "A"}]
 
-print(students)            
-print(students[0])        
-print(students[0]["name"]) 
+print(students)            # [{'name': 'A'}]
+print(students[0])         # {'name': 'A'}
+print(students[0]["name"]) # A
