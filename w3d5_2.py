6d2e726465c794661787c89eb3a1a697753c1ff5
태연 <s2401@e-mirim.hs.kr>
2025-08-22 19:56:35 +0900

    py 수정

--- w3d5_2.py
+++ w3d5_2.py
@@ -1,6 +1,11 @@
+# score = int(input("점수를 입력해주세요 : "))
+
+# if score >= 60 :
+#     print("합격!")
+# else :
+#     print("불합격..")
+
 score = int(input("점수를 입력해주세요 : "))
 
-if score >= 60 :
-    print("합격!")
-else :
-    print("불합격..")
+result = "합격!" if score >= 60 else "불합격.."
+print(result)
