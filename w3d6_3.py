2161893ce12bc68e57d3ff92a4c03ff142798221
태연 <s2401@e-mirim.hs.kr>
2025-08-24 23:49:40 +0900

    py

--- w3d6_3.py
+++ w3d6_3.py
@@ -4,3 +4,21 @@
 #     for y in range(1, 10):     
 #         print(x, "x", y, "=", x*y)
 
+# import random
+
+# answer = randint.random(1, 101)
+# count = 0
+
+# while True:
+#     guess = int(input("숫자를 맞춰보세요 (1~100): "))
+#     count += 1
+
+#     if guess == answer:
+#         print(f"정답입니다! {attempts}번 만에 맞췄어요!")
+#         break
+#     elif guess < answer:
+#         print("더 큰 수를 입력하세요.")
+#     else:
+#         print("더 작은 수를 입력하세요.")
+
+
