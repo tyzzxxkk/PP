9ab32c9d5ba70513e1a8053b3aa60dc27e43e780
태연 <s2401@e-mirim.hs.kr>
2025-08-30 23:28:42 +0900

    py

--- w4d6_2.py
+++ w4d6_2.py
@@ -1,5 +1,7 @@
 def average(scores) :
-    return sum(scores) /  len(scores)
+    # sum(scores) -> 리스트 합계
+    # len(scores) -> 리스트 원소 개수
+    return sum(scores) /  len(scores) # 평균
 
 scores = [80, 90, 70, 100]
 print("평균 점수 : ", average(scores))
